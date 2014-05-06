063e4fad55334cba83850020879c46baf86c3a09
Kerry Mui <kerry.t.mui@gmail.com>
2014-05-06 16:04:20 -0400

    Added controller and index and new view

---
+++ gimmecash/app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+	def index
+		@project = Project.all
+	end
+
+	def show
+		
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+end
