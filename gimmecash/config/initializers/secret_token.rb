ac7dbe49799d751ee4888f8725afbd3124724917
Kerry Mui <kerry.t.mui@gmail.com>
2014-05-06 14:58:13 -0400

    initial commit

---
+++ gimmecash/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gimmecash::Application.config.secret_key_base = 'e5cdb8d6e480f09c35d169d51db41141fcdea2c032072a7fda62a87ac61d4fc559572b568b4da9744461acae96019326b43e07107b647ab37ffb28d1f988a992'
---
+++ gimmecash/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gimmecash::Application.config.session_store :cookie_store, key: '_gimmecash_session'
